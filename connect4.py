878730cadba853c96c7f6def64a3775e559a97bb
Vadym Kapinos <mgistriki@gmail.com>
2026-01-20 17:21:39 +0100

    Win logic

--- connect4.py
+++ connect4.py
@@ -22,6 +22,32 @@ def get_next_open_row(board, col):
 def print_board(board):
   print(np.flip(board, 0))
 
+def winning_move(board, piece):
+  #Check horizontal location for win
+  for c in range(COLUMN_COUNTS - 3):
+    for r in range(ROW_COUNTS):
+      if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+        return True
+
+  #Check verical location for win
+  for c in range(COLUMN_COUNTS):
+    for r in range(ROW_COUNTS - 3):
+      if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+        return True
+
+  #Check positive sloped diagonal
+  for c in range(COLUMN_COUNTS - 3):
+    for r in range(ROW_COUNTS - 3):
+      if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+        return True
+
+  #Check negative sloped diagonal
+  for c in range(COLUMN_COUNTS - 3):
+    for r in range(3, ROW_COUNTS):
+      if board[r][c] == piece and board[r-1][c-1] == piece and board[r-2][c-2] == piece and board[r-3][c-3] == piece:
+        return True
+
+
 board = creat_board()
 game_over = False
 turn = 0
@@ -29,7 +55,7 @@ turn = 0
 while not game_over:
   os.system('cls' if os.name == 'nt' else 'clear')
   print_board(board)
-  
+
   # Ask for Player 1 Input
   if turn % 2 == 0:
     col = int(input("Player 1 Make your Selection (0-6):"))
@@ -38,6 +64,10 @@ while not game_over:
       row = get_next_open_row(board, col)
       drop_piece(board, row, col, 1)
 
+      if winning_move(board, 1):
+        print("PLAYER 1 Wins!!!")
+        game_over = True
+
   # Ask for Player 2 Input
   else:
     col = int(input("Player 2 Make your Selection (0-6):"))
@@ -45,6 +75,10 @@ while not game_over:
     if is_valid_location(board, col):
       row = get_next_open_row(board, col)
       drop_piece(board, row, col, 2)
+
+    if winning_move(board, 2):
+        print("PLAYER 2 Wins!!!")
+        game_over = True
 
   turn += 1
 
